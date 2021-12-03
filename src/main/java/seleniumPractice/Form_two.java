b769c5241b2d76cdd71c014a3fec03155888665f
srikanth.kareti <srikanthkareti@gmail.com>
2021-12-03 16:15:31 +0530

    Forms Registration Commit

--- src/main/java/seleniumPractice/Form_two.java
+++ src/main/java/seleniumPractice/Form_two.java
@@ -1,14 +1,12 @@
 package seleniumPractice;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
 public class Form_two
     {
         WebDriver driver;
@@ -24,29 +22,36 @@ public class Form_two
         public void Test() throws InterruptedException {
             WebElement FirstName = driver.findElement(By.id("firstName"));
             FirstName.sendKeys("Srikanth");
-
             WebElement LastName = driver.findElement(By.id("lastName"));
             LastName.sendKeys("Kareti");
-
             WebElement Email = driver.findElement(By.id("userEmail"));
             Email.sendKeys("Karetigmail.com");
             Thread.sleep(1000);
-
             WebElement male = driver.findElement(By.cssSelector(".custom-radio:nth-child(1) > .custom-control-label"));
             male.click();
-
             WebElement MobNum = driver.findElement(By.xpath("//input[@id='userNumber']"));
             MobNum.sendKeys("9654649944");
-
-            WebElement Subject = driver.findElement(By.xpath("//#genterWrapper>div:nth-of-type(2)>div:nth-of-type(3)"));
-            Subject.sendKeys("We can do it");
-
+            WebElement DOB = driver.findElement(By.id("dateOfBirthInput"));
+            DOB.click();
+            new Select(driver.findElement(By.className("react-datepicker__month-select"))).selectByVisibleText("April");
+            new Select(driver.findElement(By.className("react-datepicker__year-select"))).selectByVisibleText("1902");
+            driver.findElement(By.xpath("//div[contains(text(),'17')]")).click();
+            WebElement Subject = driver.findElement(By.id("subjectsInput"));
+            Subject.sendKeys("we can do it");
+            WebElement Hobbies = driver.findElement(By.cssSelector(".custom-checkbox:nth-child(3) > .custom-control-label"));
+            Actions actions=new Actions(driver);
+            actions.sendKeys(Keys.PAGE_DOWN).perform();
+            Thread.sleep(10000);
+            Hobbies.click();
+            actions.sendKeys(Keys.PAGE_DOWN).perform();
+            driver.findElement(By.cssSelector("#currentAddress")).sendKeys("Hyderabad");
+            driver.findElement(By.xpath("//*[@id=\"state\"]/div/div[1]")).click();
+            driver.findElement(By.id("react-select-3-option-2")).click();
+            driver.findElement(By.id("submit")).click();
         }
-
         @AfterTest
         public void AfterTest() throws InterruptedException {
-            Thread.sleep(5000);
-            driver.close();
+            //driver.close();
         }
     }
 
